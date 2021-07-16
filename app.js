97447096126b5f186dfdca6698fa1d7ef7b10195
pratik karelia <pratik25h.k@gmail.com>
2021-07-16 22:07:43 +0530

    session config

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const mongoose = require('mongoose');
+const session = require('express-session');
 const catchAsync = require('./utils/catchAsync');
 const expressError = require('./utils/expressError');
 const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override');
 const { send } = require('process');
+const bodyParser = require('body-parser')
 const Review = require('./models/review'); 
 const {campgroundSchema , reviewSchema} = require('./schemas.js')
 const campgrounds = require('./routes/campground');
@@ -27,15 +29,16 @@ db.once("open",()=>{
 app.engine('ejs',ejsMate);
 app.set('view engine', 'ejs')
 app.set('views',path.join(__dirname,'views'))
-app.use(express.urlencoded(extended=true))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
 app.use(methodOverride('_method'))
 app.get('/',(req,res)=>{
     res.render('home')
 });
 app.use('/campgrounds',campgrounds)
 app.use('/campgrounds/:id/reviews',reviews)
-app.use(express.static('public'))
-
+app.use(express.static(path.join(__dirname,'public')))
 app.use((err,req,res,next)=>{
     const {status = 500} = err;
     if(!err.message) err.message = 'Oh no, Something went wrong'
@@ -43,9 +46,22 @@ app.use((err,req,res,next)=>{
     
 })
 
-app.all('*',(req,res,next)=>{
-    next(new expressError('Page not found',404))
-})
+const sessionConfig = {
+    secret:'thisisasecret',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires: Date.now()+ 1000*60*60*27*7,
+        maxAge: 1000*60*60*27*7
+    }
+}
+
+app.use(session(sessionConfig))
+
+// app.all('*',(req,res,next)=>{
+//     next(new expressError('Page not found',404))
+// })
+
 app.listen(3000,()=>{
     console.log('On PORT 3000')
 })
