7d66ce073a27a3d175d30eb90a5e2dadaea10f16
pratik karelia <pratik25h.k@gmail.com>
2021-07-22 19:31:11 +0530

    login added

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user')
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync')
+const passport = require('passport');
 
 router.get('/register',(req,res)=>{
     res.render('users/register')
@@ -24,4 +25,9 @@ router.post('/register', catchAsync(async(req,res)=>{
 router.get('/login',(req,res)=>{
     res.render('users/login')
 })
+router.post('/login',passport.authenticate('local',{failureRedirect:'/login',failureFlash:true}),(req,res)=>{
+    req.flash('success','Welcome back')
+    res.redirect('/campgrounds')
+})
+
 module.exports = router;
