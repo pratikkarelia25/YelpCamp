35f95478f16d55d40ed25a4db9923f5eb1ef96cb
pratikkarelia25 <pratik25h.k@gmail.com>
2021-09-05 20:08:46 +0530

    security

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ if(process.env.NODE_ENV!=="production"){
 }
 const express = require('express');
 const app = express();
+const mongoSanitize = require('express-mongo-sanitize');
 const path = require('path')
 const mongoose = require('mongoose');
 const session = require('express-session');
@@ -18,6 +19,7 @@ const userRoutes = require('./routes/users')
 const passport = require('passport');
 const localStrategy = require('passport-local')
 const User = require('./models/user');
+const helmet = require('helmet');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
@@ -37,15 +39,17 @@ app.set('views',path.join(__dirname,'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(morgan('tiny'));
 app.use(flash());
+app.use(mongoSanitize());
 app.use(methodOverride('_method'))
-    app.use(express.static(path.join(__dirname,'public')))
-    app.use((err,req,res,next)=>{
-        const {status = 500} = err;
+app.use(express.static(path.join(__dirname,'public')))
+app.use((err,req,res,next)=>{
+    const {status = 500} = err;
     if(!err.message) err.message = 'Oh no, Something went wrong'
     res.status(status).render('error',{err})  
 })
 
 const sessionConfig = {
+    name: '_sess',
     secret:'thisisasecret',
     resave:false,
     saveUninitialized:true,
@@ -60,8 +64,53 @@ app.use(passport.session());
 passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
+app.use(helmet());
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+//This is the array that needs added to
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dsdxhieag/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 app.use((req, res, next) => {
+    console.log(req.query)
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
@@ -72,6 +121,9 @@ app.use('/campgrounds',campgroundsRoutes)
 app.use('/campgrounds/:id/reviews',reviewsRoutes)
 app.use('/',userRoutes);
 
+app.get('/',(req,res)=>{
+    res.render('home')
+})
 // app.all('*',(req,res,next)=>{
 //     next(new expressError('Page not found',404))
 // })
